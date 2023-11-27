f6a6bd91dd5aae7c8b24f725c844146dc67cb76d
kaiki212 <kevinrioja216@gmail.com>
2023-11-26 22:26:22 -0400

    Correcciones menores

--- src/main/java/com/mycompany/proyectoftp/PaginaPrincipal.java
+++ src/main/java/com/mycompany/proyectoftp/PaginaPrincipal.java
@@ -218,6 +218,7 @@ public class PaginaPrincipal extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
+        gestion.instalarVSFTP();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
